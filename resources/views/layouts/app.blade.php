3e41c7e2d3e0113148befc7f35242fadb9427132
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-19 01:22:01 -0300

    Add Laracast Flash Messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,4 +81,8 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/i18n/jquery-ui-i18n.min.js"></script>
 @yield('scripts')
+<!--Para Flash Menssages -->
+<script>
+    $('#flash-overlay-modal').modal();
+</script>
 </html>
