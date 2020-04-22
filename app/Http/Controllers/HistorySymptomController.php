9f00569eed81260db914ac81ab5e21692e5ba0cc
BAUBYTE <paredbaez.martin@gmail.com>
2020-04-21 23:19:07 -0300

    Add Security Controller

--- app/Http/Controllers/HistorySymptomController.php
+++ app/Http/Controllers/HistorySymptomController.php
@@ -8,6 +8,10 @@ use App\HistorySymptom;
 
 class HistorySymptomController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
